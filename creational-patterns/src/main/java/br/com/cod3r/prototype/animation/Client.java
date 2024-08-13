5a4e8f81689c6cbdf3bc1522bafacc5e9891e4ad
Gabriel Luciano <gabriel-luciano@outlook.com>
2024-08-13 15:27:55 -0300

    Prototype (shallow clone)

--- creational-patterns/src/main/java/br/com/cod3r/prototype/animation/Client.java
+++ creational-patterns/src/main/java/br/com/cod3r/prototype/animation/Client.java
@@ -1,32 +1,63 @@
 package br.com.cod3r.prototype.animation;
 
+import br.com.cod3r.prototype.animation.model.Person;
+import br.com.cod3r.prototype.animation.model.PersonSamples;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import br.com.cod3r.prototype.animation.model.Person;
-
 public class Client {
-	private static List<Person> frames = new ArrayList<Person>();
-	
-	public static void animate() throws InterruptedException {
-		System.out.println("*********************************");
-		System.out.println("* ");
-		System.out.println("* Adjust your screen's height!");
-		System.out.println("* ");
-		System.out.print("*********************************");
-		Thread.sleep(3000);
-		for(Person frame: frames) {
-			frame.draw();
-			Thread.sleep(500);
-		}
-		System.out.println("**********************");
-		System.out.println("* ");
-		System.out.println("* The End!");
-		System.out.println("* ");
-		System.out.print("**********************");
-	}
-	
-	public static void main(String[] args) {
-		
-	}
+    private static final List<Person> frames = new ArrayList<>();
+
+    public static void animate() throws InterruptedException {
+        System.out.println("*********************************");
+        System.out.println("* ");
+        System.out.println("* Adjust your screen's height!");
+        System.out.println("* ");
+        System.out.print("*********************************");
+        Thread.sleep(3000);
+        for (Person frame : frames) {
+            frame.draw();
+            Thread.sleep(500);
+        }
+        System.out.println("**********************");
+        System.out.println("* ");
+        System.out.println("* The End!");
+        System.out.println("* ");
+        System.out.print("**********************");
+    }
+
+    public static Person addLeft(Person p) throws CloneNotSupportedException {
+        Person person = (Person) p.clone();
+        person.left();
+        frames.add(person);
+        return person;
+    }
+
+    public static Person addRight(Person p) throws CloneNotSupportedException {
+        Person person = (Person) p.clone();
+        person.right();
+        frames.add(person);
+        return person;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Person person = new PersonSamples().get("stickMan");
+
+        frames.add(person);
+        person = addLeft(person);
+        person = addLeft(person);
+        person = addLeft(person);
+        person = addRight(person);
+        person = addRight(person);
+        person = addRight(person);
+        person = addRight(person);
+        person = addRight(person);
+        person = addRight(person);
+        person = addLeft(person);
+        person = addLeft(person);
+        addLeft(person);
+
+        Client.animate();
+    }
 }
