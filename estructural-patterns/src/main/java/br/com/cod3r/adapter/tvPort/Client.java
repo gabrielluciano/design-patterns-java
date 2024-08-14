99307bfb7154942f3dd0e7f4cda17225def6df79
Gabriel Luciano <gabriel-luciano@outlook.com>
2024-08-13 22:49:06 -0300

    Adapter (Class)

--- estructural-patterns/src/main/java/br/com/cod3r/adapter/tvPort/Client.java
+++ estructural-patterns/src/main/java/br/com/cod3r/adapter/tvPort/Client.java
@@ -1,5 +1,6 @@
 package br.com.cod3r.adapter.tvPort;
 
+import br.com.cod3r.adapter.tvPort.adapters.HDMIToOldMonitorAdapter;
 import br.com.cod3r.adapter.tvPort.adapters.HDMIToVGAAdapter;
 import br.com.cod3r.adapter.tvPort.devices.Computer;
 import br.com.cod3r.adapter.tvPort.devices.OldMonitor;
@@ -7,21 +8,24 @@ import br.com.cod3r.adapter.tvPort.devices.TV;
 
 public class Client {
 
-	public static void main(String[] args) {
-		Computer pc = new Computer();
-		TV tv = new TV();
-		pc.connectPort(tv);
-		pc.sendImageAndSound("Cat and rainbow", "Nyan cat song");
-		
-		System.out.println("\n------ Monitor ----------");
-		OldMonitor oldMonitor = new OldMonitor();
-		// pc.connectPort(oldMonitor); // Not possible since oldMonitor doesn't implement HDMI
-		HDMIToVGAAdapter adapter = new HDMIToVGAAdapter(oldMonitor);
-		pc.connectPort(adapter);
-		pc.sendImageAndSound("Cat and rainbow", "Nyan cat song");
+    public static void main(String[] args) {
+        Computer pc = new Computer();
+        TV tv = new TV();
+        pc.connectPort(tv);
+        pc.sendImageAndSound("Cat and rainbow", "Nyan cat song");
 
-		System.out.println("\n------ Monitor Class Adapter ----------");
-		
+        System.out.println("\n------ Monitor ----------");
+        OldMonitor oldMonitor = new OldMonitor();
+        // pc.connectPort(oldMonitor); // Not possible since oldMonitor doesn't implement HDMI
+        HDMIToVGAAdapter adapter = new HDMIToVGAAdapter(oldMonitor);
+        pc.connectPort(adapter);
+        pc.sendImageAndSound("Cat and rainbow", "Nyan cat song");
 
-	}
+        System.out.println("\n------ Monitor Class Adapter ----------");
+        Computer NASApC = new Computer();
+        HDMIToOldMonitorAdapter oldMonitorAdapter = new HDMIToOldMonitorAdapter();
+        NASApC.connectPort(oldMonitorAdapter);
+        NASApC.sendImageAndSound("Cat and rainbow", "Nyan cat song");
+
+    }
 }
