f87b24fcd8918f61d2716495fe52cdbde35be751
Gabriel Luciano <gabriel-luciano@outlook.com>
2024-08-13 23:20:27 -0300

    Adapter (Hexagonal Architecture)

--- estructural-patterns/src/main/java/br/com/cod3r/adapter/hexagonal/application/UserRest.java
+++ estructural-patterns/src/main/java/br/com/cod3r/adapter/hexagonal/application/UserRest.java
@@ -1,17 +1,36 @@
 package br.com.cod3r.adapter.hexagonal.application;
 
+import br.com.cod3r.adapter.hexagonal.core.model.User;
+import br.com.cod3r.adapter.hexagonal.core.ports.UserRepository;
+import br.com.cod3r.adapter.hexagonal.core.usecases.UserService;
+import br.com.cod3r.adapter.hexagonal.infrastructure.UserMemoryDatabaseAdapter;
+
+import java.util.List;
 import java.util.Map;
 
 public class UserRest {
-	
-	public UserRest() {
-	}
 
-	public Integer post(Map<String, String> values) {
-		return 201;
-	}
-	
-	public Integer get() {
-		return 200;
-	}
+    private final UserService userFacade;
+
+    public UserRest() {
+        UserRepository userRepository = new UserMemoryDatabaseAdapter();
+        userFacade = new UserService(userRepository);
+    }
+
+    public Integer post(Map<String, String> values) {
+        try {
+            User user = new User(values.get("name"), values.get("email"), values.get("password"));
+            userFacade.saveUser(user);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return 400;
+        }
+        return 201;
+    }
+
+    public Integer get() {
+        List<User> users = userFacade.getUsers();
+        users.stream().forEach(System.out::println);
+        return 200;
+    }
 }
--- estructural-patterns/src/main/java/br/com/cod3r/adapter/hexagonal/core/usecases/UserService.java
+++ estructural-patterns/src/main/java/br/com/cod3r/adapter/hexagonal/core/usecases/UserService.java
@@ -1,5 +1,25 @@
 package br.com.cod3r.adapter.hexagonal.core.usecases;
 
+import br.com.cod3r.adapter.hexagonal.core.model.User;
+import br.com.cod3r.adapter.hexagonal.core.ports.UserRepository;
+
+import java.util.List;
+
 public class UserService {
-	
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void saveUser(User user) {
+        boolean alreadyUsedEmail = userRepository.getAll().stream()
+                .anyMatch(userDb -> userDb.getEmail().equalsIgnoreCase(user.getEmail()));
+        if (alreadyUsedEmail) throw new RuntimeException("Email address already used");
+        userRepository.save(user);
+    }
+
+    public List<User> getUsers() {
+        return userRepository.getAll();
+    }
 }
