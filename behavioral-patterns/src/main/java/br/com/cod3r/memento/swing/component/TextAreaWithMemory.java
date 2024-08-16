67abbdda3808e685b1bef47cfd489b27bc17f6f7
Gabriel Luciano <gabriel-luciano@outlook.com>
2024-08-16 11:29:29 -0300

    Memento

--- behavioral-patterns/src/main/java/br/com/cod3r/memento/swing/component/TextAreaWithMemory.java
+++ behavioral-patterns/src/main/java/br/com/cod3r/memento/swing/component/TextAreaWithMemory.java
@@ -1,13 +1,33 @@
 package br.com.cod3r.memento.swing.component;
 
-import javax.swing.JTextArea;
+import br.com.cod3r.memento.swing.memory.Memento;
+
+import javax.swing.*;
 
 public class TextAreaWithMemory extends JTextArea {
-	private static final long serialVersionUID = 2393587108160522742L;
-	
-	public TextAreaWithMemory(int rows, int columns) {
-		super(rows, columns);
-	}
+    private static final long serialVersionUID = 2393587108160522742L;
 
+    public TextAreaWithMemory(int rows, int columns) {
+        super(rows, columns);
+    }
 
+    public TextAreaMemento save() {
+        return new TextAreaMemento(getText());
+    }
+
+    public void restore(TextAreaMemento memento) {
+        setText(memento.getState());
+    }
+
+    public static final class TextAreaMemento implements Memento {
+        private final String text;
+
+        public TextAreaMemento(String text) {
+            this.text = text;
+        }
+
+        private String getState() {
+            return text;
+        }
+    }
 }
