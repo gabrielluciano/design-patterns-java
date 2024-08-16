97e9e7df68e5a04e5315e029a1c78dfef54b450b
Gabriel Luciano <gabriel-luciano@outlook.com>
2024-08-16 12:13:53 -0300

    Command (alexa - before)

---
+++ behavioral-patterns/src/main/java/br/com/cod3r/command/alexa/before/Client.java
@@ -0,0 +1,14 @@
+package br.com.cod3r.command.alexa.before;
+
+import br.com.cod3r.command.alexa.before.alexa.Alexa;
+
+public class Client {
+
+    public static void main(String[] args) {
+        Alexa alexa = new Alexa();
+        alexa.sendRequest("Turn on the Living room light");
+        alexa.sendRequest("Turn off the Living room light");
+        alexa.sendRequest("Turn on the Kitchen light");
+        alexa.sendRequest("Turn off the Kitchen light");
+    }
+}
--- behavioral-patterns/src/main/java/br/com/cod3r/command/alexa/before/alexa/Alexa.java
+++ behavioral-patterns/src/main/java/br/com/cod3r/command/alexa/before/alexa/Alexa.java
@@ -1,10 +1,10 @@
-package br.com.cod3r.command.alexa.alexa;
+package br.com.cod3r.command.alexa.before.alexa;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import br.com.cod3r.command.alexa.lights.PhillipsHueLight;
-import br.com.cod3r.command.alexa.lights.XiaomiLight;
+import br.com.cod3r.command.alexa.before.lights.PhillipsHueLight;
+import br.com.cod3r.command.alexa.before.lights.XiaomiLight;
 
 public class Alexa {
 	private Map<String, Object> integrations;
@@ -17,19 +17,19 @@ public class Alexa {
 
 	public void sendRequest(String request) {
 		if(request.equals("Turn on the Living room light")) {
-			System.out.println("Turnning on the Living room light");
+			System.out.println("Turning on the Living room light");
 			PhillipsHueLight light = (PhillipsHueLight) integrations.get("LivingRoomLight");
 			light.turnOn();
 		} else if(request.equals("Turn off the Living room light")) {
-			System.out.println("Turnning off the Living room light");
+			System.out.println("Turning off the Living room light");
 			PhillipsHueLight light = (PhillipsHueLight) integrations.get("LivingRoomLight");
 			light.turnOff();
 		} else if(request.equals("Turn on the Kitchen light")) {
-			System.out.println("Turnning on the Kitchen light");
+			System.out.println("Turning on the Kitchen light");
 			XiaomiLight light = (XiaomiLight) integrations.get("KitchenLight");
 			light.turnOn();
 		} else if(request.equals("Turn off the Kitchen light")) {
-			System.out.println("Turnning off the Kitchen light");
+			System.out.println("Turning off the Kitchen light");
 			XiaomiLight light = (XiaomiLight) integrations.get("KitchenLight");
 			light.turnOff();
 		} else {
