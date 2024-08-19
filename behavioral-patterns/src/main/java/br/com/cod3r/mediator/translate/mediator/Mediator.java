5a827ce8a344b830c049659d704a430499e06068
Gabriel Luciano <gabriel-luciano@outlook.com>
2024-08-19 10:31:52 -0300

    Mediator (chat)

--- behavioral-patterns/src/main/java/br/com/cod3r/mediator/translate/mediator/Mediator.java
+++ behavioral-patterns/src/main/java/br/com/cod3r/mediator/translate/mediator/Mediator.java
@@ -2,26 +2,27 @@ package br.com.cod3r.mediator.translate.mediator;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 import br.com.cod3r.mediator.translate.model.User;
 
 public abstract class Mediator {
-	protected Map<String, User> users = new HashMap<String, User>();
+	protected Map<String, User> users = new HashMap<>();
 
 	public void addUser(User user) {
-		System.out.println(String.format("'%s' is in the room", user.getName()));
+		System.out.printf("'%s' is in the room%n", user.getName());
 		users.put(user.getName(), user);
 	}
 	
 	public void removeUser(User user) {
-		System.out.println(String.format("'%s' is out", user.getName()));
+		System.out.printf("'%s' is out%n", user.getName());
 		users.remove(user.getName());
 	}
 
 	public void sendMessage(String message, User to, User from) {
 		if (to == null) {
 			for (User user : users.values()) {
-				if (user.getName() == from.getName())
+				if (Objects.equals(user.getName(), from.getName()))
 					continue;
 				user.receiveMessage(getMessage(message, user, from), from);
 			}
--- behavioral-patterns/src/main/java/br/com/cod3r/mediator/translate/model/User.java
+++ behavioral-patterns/src/main/java/br/com/cod3r/mediator/translate/model/User.java
@@ -3,34 +3,35 @@ package br.com.cod3r.mediator.translate.model;
 import br.com.cod3r.mediator.translate.mediator.Mediator;
 
 public abstract class User {
-	final protected String name;
-	final protected Language language;
-	final protected Mediator mediator;
-	
-	public User(String name, Mediator mediator, Language language) {
-		this.name = name;
-		this.mediator = mediator;
-		this.language = language;
-	}
+    protected final String name;
+    protected final Language language;
+    protected final Mediator mediator;
 
-	public String getName() {
-		return name;
-	}
-	public Language getLanguage() {
-		return language;
-	}
-	
-	public void sendMessage(String message) {
-		this.sendMessage(message, null);
-	}
+    protected User(String name, Mediator mediator, Language language) {
+        this.name = name;
+        this.mediator = mediator;
+        this.language = language;
+    }
 
-	public void sendMessage(String message, User to) {
-		String receiverName = to != null ? to.getName() : "ALL";
-		System.out.println(String.format("'%s' is sending the message '%s' to '%s'", name, message, receiverName));
-		mediator.sendMessage(message, to, this);
-	}
-	
-	public void receiveMessage(String message, User from) {
-		System.out.println(String.format("'%s' has recieved the message '%s' from '%s'", name, message, from.getName()));
-	}
+    public String getName() {
+        return name;
+    }
+
+    public Language getLanguage() {
+        return language;
+    }
+
+    public void sendMessage(String message) {
+        this.sendMessage(message, null);
+    }
+
+    public void sendMessage(String message, User to) {
+        String receiverName = to != null ? to.getName() : "ALL";
+        System.out.printf("'%s' is sending the message '%s' to '%s'%n", name, message, receiverName);
+        mediator.sendMessage(message, to, this);
+    }
+
+    public void receiveMessage(String message, User from) {
+        System.out.printf("'%s' has received the message '%s' from '%s'%n", name, message, from.getName());
+    }
 }
