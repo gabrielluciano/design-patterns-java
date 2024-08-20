d8d95a667f073370274d909741d7ac61c25bce8c
Gabriel Luciano <gabriel-luciano@outlook.com>
2024-08-20 09:19:46 -0300

    State (pacman)

--- behavioral-patterns/src/main/java/br/com/cod3r/state/pacman/game/Game.java
+++ behavioral-patterns/src/main/java/br/com/cod3r/state/pacman/game/Game.java
@@ -13,20 +13,23 @@ public class Game {
     public void addListener(GameEventListener listener) {
         listeners.add(listener);
     }
-    
-    public void startLoop() throws IOException {
-		BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("Hi! I'm a good ghost. Help me to catch that bad PacMan...");
-		while(exit == false) {
-			System.out.println("Choose what will happen next...\n1: Spot the PacMan\n"
-					+ "2: Lose the PacMan\n3: PacMan turns special\n4: PacMan is regular again\n"
-					+ "5: Eaten by PacMan\n6: Reach the base\n0: Exit");
-			String option = buffer.readLine();
-            System.out.println("\n\n");
-            listeners.forEach(l -> l.onAction(option));
-			System.out.println("\n\n");
-		}
-		System.out.println("Ok! Let's play again later?");
+
+    public void startLoop() {
+        try (BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.println("Hi! I'm a good ghost. Help me to catch that bad PacMan...");
+            while (!exit) {
+                System.out.println("Choose what will happen next...\n1: Spot the PacMan\n"
+                        + "2: Lose the PacMan\n3: PacMan turns special\n4: PacMan is regular again\n"
+                        + "5: Eaten by PacMan\n6: Reach the base\n0: Exit");
+                String option = buffer.readLine();
+                System.out.println("\n\n");
+                listeners.forEach(l -> l.onAction(option));
+                System.out.println("\n\n");
+            }
+            System.out.println("Ok! Let's play again later?");
+        } catch (IOException exception) {
+            System.out.println("Sorry! An error happened!");
+        }
     }
 
     public void stopLoop() {
