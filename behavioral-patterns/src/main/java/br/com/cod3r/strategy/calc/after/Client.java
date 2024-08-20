4c89d68f1b51d0e622e8201c6c53980294142be5
Gabriel Luciano <gabriel-luciano@outlook.com>
2024-08-20 09:50:03 -0300

    Strategy (calculator - after)

---
+++ behavioral-patterns/src/main/java/br/com/cod3r/strategy/calc/after/Client.java
@@ -0,0 +1,18 @@
+package br.com.cod3r.strategy.calc.after;
+
+import br.com.cod3r.strategy.calc.after.strategies.AdditionOperation;
+import br.com.cod3r.strategy.calc.after.strategies.DivisionOperation;
+import br.com.cod3r.strategy.calc.after.strategies.MultiplicationOperation;
+import br.com.cod3r.strategy.calc.after.strategies.SubtractionOperation;
+
+public class Client {
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator(10, 2);
+        System.out.println(calc.getResult(new AdditionOperation()));
+        System.out.println(calc.getResult(new SubtractionOperation()));
+        System.out.println(calc.getResult(new MultiplicationOperation()));
+        System.out.println(calc.getResult(new DivisionOperation()));
+        System.out.println(calc.getResult((num1, num2) -> (int) Math.pow(num1, num2)));
+    }
+}
